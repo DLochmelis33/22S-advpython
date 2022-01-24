70e85e0caa05cd34c377a1ee7ba2e53be8c780ba
dlochmelis33 <dlochmelis@gmail.com>
2022-01-24 13:22:14 +0300

    Complete 'Easy' part

---
+++ hw01/fib.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+def fib(n: int) -> List[int]:
+    if n < 0:
+        return []
+    a = 0
+    b = 1
+    result = []
+    while n > 0:
+        result.append(a)
+        tmp = a
+        a = a + b
+        b = tmp
+        n = n - 1
+    return result
+
+
+if __name__ == '__main__':
+    print(fib(int(input())))
